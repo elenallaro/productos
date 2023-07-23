d52f59588720c2d950a5d1a987757f6f41e492d2
marianarui <mariana.4ruiz8@gmail.com>
2023-07-23 04:14:37 -0600

    Se agregó modal con descripción.

--- productos_ejercicio/js/main.js
+++ productos_ejercicio/js/main.js
@@ -6,6 +6,7 @@
     let img = document.getElementById("img");
     let title = document.getElementById("title");
     let text = document.getElementById("text");
+    let btn= document.getElementById("btn");
     let card = document.getElementById("card");
 
     
@@ -43,7 +44,30 @@ function createCards(data){
         <img src= ${producto.image} id= "img" class="card-img-top" alt="...">
         <div class="card-body" >
             <h5 class="card-title" id="title">${producto.title}</h5>
-            <p class="card-text" id="text">${producto.description}</p>
+            <p class="card-text" id="price">$${producto.price} USD</p>
+        </div>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Descripción
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">${producto.title}</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+            </div>
+            <div class="modal-body">
+                ${producto.description}
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary btn2" data-bs-dismiss="modal">Cerrar</button>
+            </div>
+            </div>
+        </div>
+        </div>
         </div>`);
 
         
